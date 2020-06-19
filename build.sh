9fd83f59a2166a8fcc80a9b36d665594f416544a
vaibhavmehtaa <vaibhav.mehta@vogo.in>
2020-06-20 01:57:02 +0530

    :smiling_imp: update: (update the pod autoscale file)

--- build.sh
+++ build.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
 # we gonna follow these steps in ci/cd pipeline
-tag=$1
+# tag=$1
 
-npm run build
-aws s3 sync dist/img s3://assets.everwell/img  --acl public-read
-aws s3 sync dist/css s3://assets.everwell/css  --acl public-read
-aws s3 sync dist/js s3://assets.everwell/js  --acl public-read
-docker build -t vaibhavmehta/pwa-weather-app:$tag .
-docker push vaibhavmehta/pwa-weather-app:$tag
+# npm run build
+# aws s3 sync dist/img s3://assets.everwell/img  --acl public-read
+# aws s3 sync dist/css s3://assets.everwell/css  --acl public-read
+# aws s3 sync dist/js s3://assets.everwell/js  --acl public-read
+# docker build -t vaibhavmehta/pwa-weather-app:$tag .
+# docker push vaibhavmehta/pwa-weather-app:$tag
 
-
+# sh /Users/apple/prac/helm-charts/build.sh $tag
 curl -v -X GET http://localhost:8080/job/PWA-weather-frontend-production-trigger/build
